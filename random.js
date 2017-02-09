75e1bf9b23ad90522e03a584424567066f86a49c
Joshua Banuelos <xxdamagexx0524@gmail.com>
2017-02-09 10:16:00 -0800

    pi

--- random.js
+++ random.js
@@ -5,7 +5,8 @@ var namespace = "http://www.w3.org/2000/svg"
 // You need to use at least 3 different types of shape.
 // Remember, for the shapes to show up on the canvas, you'll need to CALL the function.
 function createFirstScene() {
-    
+    makeRect(50, 50, 50, 50, "black", 1)
+    makeImage("https://img.clipartfest.com/94f1d48ddfcb0af2f080253b7bd127e8_final-result-mario-face-clip-art_617-616.png",50,50,50,50,1)
 }
 
 
@@ -13,7 +14,8 @@ function createFirstScene() {
 // You need to use at least 3 different types of shape.
 // Remember, for the shapes to show up on the canvas, you'll need to CALL the function.
 function createSecondScene() {
-    
+    makeRect(50, 50, 50, 50, "grey", 0.90)
+   makeImage("http://www.decalninja.com/images/12936670968802061501430.jpeg",50,50,50,50,1) 
 }
 
 
@@ -21,7 +23,8 @@ function createSecondScene() {
 // You need to use at least 3 different types of shape.
 // Remember, for the shapes to show up on the canvas, you'll need to CALL the function.
 function createThirdScene() {
-    
+    makeRect(50, 50, 50, 50, "black", 0.90)
+    makeImage("http://vignette2.wikia.nocookie.net/raresonicthehedgehoggames/images/2/23/Classic_sonic_face.svg/revision/latest?cb=20120220083439",50,50,50,50,1)
 }
 
 
@@ -35,16 +38,15 @@ function createRandomScene() {
     var rando = Math.random()
     // If the number is less than 0.33, call the function to create your first scene.
     if(rando<0.33){
-     createFirstScene()   
+     createFirstScene()    
+    
     }
-    
-    
     // Else, if the number is less than 0.67, call the function to create your second scene.
-    else{
-    createSecondScene
+    else if (rando<0.67){
+    createSecondScene()
     
     }// Else, call the function to create your third scene.
-    else{
+    else {
     createThirdScene()
     }
 }
